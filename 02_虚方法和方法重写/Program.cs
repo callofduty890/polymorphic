b6e6eeb9d25a6be43c2692935c5c7639397b3403
俊华 蔡 <1032031912@qq.com>
2020-09-22 10:02:45 +0800

    多态的虚方法与重写方法

--- 02_虚方法和方法重写/Program.cs
+++ 02_虚方法和方法重写/Program.cs
@@ -31,11 +31,39 @@ namespace _02_虚方法和方法重写
         }
     }
 
+    public class Shere : Dimension //派生类：球体
+    {
+        public Shere(double r) : base(r, 0) { }
+        public override double Area()//球体的表面积
+        {
+            return 4 * Math.PI * x * x;
+        }
+    }
+
+    public class Cylinder : Dimension //派生类:圆柱体
+    {
+        public Cylinder(double r,double h) : base(r, h) { }
+        public override double Area()//圆柱表面积
+        {
+            return 2*Math.PI*x*x+2*Math.PI*x*y;
+        }
+
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            double r = 3.0, h = 5.0;
+            Dimension[] dimensions = { new Circle(r), new Shere(r), new Cylinder(r, h) };
+
+            foreach (Dimension item in dimensions)
+            {
+                //显示不同类型状态的表面积
+                Console.WriteLine("Area of {0}={1:0.00}", item.GetType(), item.Area());
+            }
+
+            Console.ReadKey();
         }
     }
 }
